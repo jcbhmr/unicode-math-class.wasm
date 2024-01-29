bd5a80c412726d01a08bb8433e7d4dd516631d2a
Jacob Hummer <jcbhmr@outlook.com>
2024-01-29 22:10:39 +0000

    https://github.com/bytecodealliance/wit-bindgen/issues/822

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,23 @@
+mod bindings;
 mod my_macros;
 
-cargo_component_bindings::generate!();
 use my_macros::rs_wit_parallel_enum;
 use unicode_math_class;
 
 pub struct Component;
-impl bindings::Guest for Component {
+impl bindings::exports::typst_community::unicode_math_class::types::Guest for Component {
     fn revision() -> u8 {
         unicode_math_class::REVISION
     }
-    fn class(c: String) -> Option<bindings::MathClass> {
+    fn class(
+        c: String,
+    ) -> Option<bindings::exports::typst_community::unicode_math_class::types::MathClass> {
         let c = c.chars().next().expect("non-empty string");
         unicode_math_class::class(c).map(|x| x.into())
     }
 }
 
-rs_wit_parallel_enum!(unicode_math_class::MathClass, bindings::MathClass, {
+rs_wit_parallel_enum!(unicode_math_class::MathClass, bindings::exports::typst_community::unicode_math_class::types::MathClass, {
     Normal,
     Alphabetic,
     Binary,
